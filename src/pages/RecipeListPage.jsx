4fb7ee61d8f04a74498b64d46313fa1c49940445
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-22 17:50:29 +0200

    imported RecipeItemCard added item and key

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,5 +1,6 @@
 import { Center } from "@chakra-ui/react";
 import { data } from "../utils/data";
+import { RecipeItemCard } from "../components/RecipeItemCard";
 
 export const RecipeListPage = () => {
   // You can play around with the console log, but ultimately remove it once you are done
@@ -8,6 +9,7 @@ export const RecipeListPage = () => {
   return (
     <>
       <Center h="100vh" flexDir="column" bgColor="gray.900"></Center>
+      <RecipeItemCard item={item} key={item.hits} />
     </>
   );
 };
