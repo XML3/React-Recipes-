2740d335b6ee52be024ab5fc3f4de61668560684
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-22 18:17:39 +0200

    extracted data.hits in recipes variable,Render+Key

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,15 +1,21 @@
-import { Center } from "@chakra-ui/react";
+import { Center, SimpleGrid } from "@chakra-ui/react";
 import { data } from "../utils/data";
 import { RecipeItemCard } from "../components/RecipeItemCard";
 
 export const RecipeListPage = () => {
   // You can play around with the console log, but ultimately remove it once you are done
   //console.log(data.hits[0].recipe.label);
+  const recipes = data.hits;
 
   return (
     <>
-      <Center h="100vh" flexDir="column" bgColor="gray.900"></Center>
-      <RecipeItemCard item={item} key={item.hits} />
+      <Center h="100vh" flexDir="column" bgColor="gray.900">
+        <SimpleGrid columns={4} gap={8}>
+          {recipes.map((item, index) => (
+            <RecipeItemCard key={item.recipe.url} item={item} />
+          ))}
+        </SimpleGrid>
+      </Center>
     </>
   );
 };
