e94b01841212cb516eea1736ff725773e7d13db9
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-22 18:20:57 +0200

    added comments to code

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -5,12 +5,15 @@ import { RecipeItemCard } from "../components/RecipeItemCard";
 export const RecipeListPage = () => {
   // You can play around with the console log, but ultimately remove it once you are done
   //console.log(data.hits[0].recipe.label);
+
+  // Extracted data.hits and placed in recipes variable
   const recipes = data.hits;
 
   return (
     <>
       <Center h="100vh" flexDir="column" bgColor="gray.900">
         <SimpleGrid columns={4} gap={8}>
+          {/* Mapped throught recipe object as the "item" prop for RecipeItemCard and used unique Key prop for each recipe item (index or recipe URL) */}
           {recipes.map((item, index) => (
             <RecipeItemCard key={item.recipe.url} item={item} />
           ))}
