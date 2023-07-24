90d2c0b8c6bcc2f232fcc4ff6b00812ac168b1e5
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-24 20:28:38 +0200

    created conditional in return clickFn, useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { RecipeListPage } from "./pages/RecipeListPage";
+// import { RecipeSearch } from "./components/RecipeSearch";
 import { data } from "./utils/data";
+import { useState } from "react";
+import { RecipePage } from "./components/RecipePage";
 
 export const App = () => {
   // Your state code here
+  const [selectedItem, setSelectedItem] = useState();
 
   return (
     <>
-      <RecipeListPage />
+      {selectedItem ? (
+        <RecipePage item={selectedItem} clickFn={setSelectedItem} />
+      ) : (
+        <RecipeListPage selectedItem={selectedItem} clickFn={setSelectedItem} />
+      )}
+      {/* <RecipeSearch data={data} clickFn={setSelectedItem} /> */}
+      {/* Recipe Search is rendering the page twice, chek and fix */}
     </>
   );
 };
