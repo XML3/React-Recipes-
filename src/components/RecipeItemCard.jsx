405c249bc35a0cc42d41a804f95f33939effc152
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-24 20:27:28 +0200

    Added Flex container

--- src/components/RecipeItemCard.jsx
+++ src/components/RecipeItemCard.jsx
@@ -1,4 +1,12 @@
-import { Card, CardBody, Image, Stack, Heading, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
 
 export const RecipeItemCard = ({ item, clickFn }) => {
   const {
@@ -31,24 +39,28 @@ export const RecipeItemCard = ({ item, clickFn }) => {
           <Heading size="md" mt={4}>
             {label}
           </Heading>
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            View Recipe
-          </a>
-          {dietLabels && (
-            <Text bgColor="blue.200">Diet Label: {dietLabels}</Text>
-          )}
-          {cautions && (
-            <Text bgColor="pink.400" fontWeight="bold">
-              Caution: {cautions}
-            </Text>
-          )}
-          {isVegan ? (
-            <Text bgColor="lime">Vegan</Text>
-          ) : isVegetarian ? (
-            <Text bgColor="lime">Vegetarian</Text>
-          ) : null}
-          <Text>Meal Type: {mealType}</Text>
-          <Text>Dish Type: {dishType}</Text>
+          <Stack size="sm">
+            <Flex gap={1} w={["full", "75%"]} flexWrap="wrap" flexDir="column">
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                View Recipe
+              </a>
+              {dietLabels && (
+                <Text bgColor="blue.200">Diet Label: {dietLabels}</Text>
+              )}
+              {cautions && (
+                <Text bgColor="pink.400" fontWeight="bold">
+                  Caution: {cautions}
+                </Text>
+              )}
+              {isVegan ? (
+                <Text bgColor="lime">Vegan</Text>
+              ) : isVegetarian ? (
+                <Text bgColor="lime">Vegetarian</Text>
+              ) : null}
+              <Text>Meal Type: {mealType}</Text>
+              <Text>Dish Type: {dishType}</Text>
+            </Flex>
+          </Stack>
         </CardBody>
       </Card>
     </>
