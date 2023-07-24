8f53305cf6c8e2f93297b33fcfd60a05c83f0152
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-24 20:29:16 +0200

    Created the search function, still testing.

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -1,3 +1,25 @@
-export const RecipeSearch = () => {
-  return <></>;
+import { useState } from "react";
+import { data } from "../utils/data";
+import { TextInput } from "./ui/TextInput";
+import { RecipeListPage } from "../pages/RecipeListPage";
+
+export const RecipeSearch = ({ data, clickFn }) => {
+  const [searchField, setSearchField] = useState("");
+
+  const matchedRecipes = data.hits.filter((item) => {
+    const { label, dietLabels, cautions, healthLabels } = item.recipe;
+    return label.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  const handleChange = (event) => {
+    setSearchField(event.target.value);
+  };
+
+  return (
+    <>
+      <label>Search for a recipe:</label>
+      <TextInput changeFn={handleChange} w={200} mb={8} />
+      <RecipeListPage clickFn={clickFn} items={matchedRecipes} />
+    </>
+  );
 };
