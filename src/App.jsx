c91e5f40ba3277ca70ffb481fe57773c302e1287
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-25 21:36:16 +0200

    Modified the styling to responsive and placed in Box

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,42 @@ import { RecipeSearch } from "./components/RecipeSearch";
 import { data } from "./utils/data";
 import { useState } from "react";
 import { RecipePage } from "./components/RecipePage";
+import { Box, Center, Heading } from "@chakra-ui/react";
 
 export const App = () => {
   // Your state code here
   const [selectedItem, setSelectedItem] = useState();
-
   //new state for filtered recipes
   const [filteredRecipes, setFilteredRecipes] = useState(data.hits);
+
   // handle filter recipe from RecipeSearch
   const handleFilteredRecipes = (recipes) => {
     setFilteredRecipes(recipes);
   };
   return (
-    <>
-      {selectedItem ? (
-        <RecipePage item={selectedItem} clickFn={setSelectedItem} />
-      ) : (
-        <>
-          <RecipeSearch
-            data={data}
-            clickFn={setSelectedItem}
-            handleFilteredRecipes={handleFilteredRecipes}
-          />
-          <RecipeListPage
-            selectedItem={selectedItem}
-            clickFn={setSelectedItem}
-            items={filteredRecipes}
-          />
-        </>
-      )}
-    </>
+    <Box>
+      <Center h="100hv" flexDir="column" bgColor="gray.900" color="whitesmoke">
+        <Heading as="h1" fontSize={["3xl", "4xl", "6xl"]}>
+          Recipe Checker
+        </Heading>
+
+        {selectedItem ? (
+          <RecipePage item={selectedItem} clickFn={setSelectedItem} />
+        ) : (
+          <>
+            <RecipeSearch
+              items={filteredRecipes}
+              clickFn={setSelectedItem}
+              handleFilteredRecipes={handleFilteredRecipes}
+            />
+            <RecipeListPage
+              selectedItem={selectedItem}
+              clickFn={setSelectedItem}
+              items={filteredRecipes}
+            />
+          </>
+        )}
+      </Center>
+    </Box>
   );
 };
