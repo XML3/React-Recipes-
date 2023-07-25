5eed42ecfb85bdcdd6a5a69b06432f5adb3fd355
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-25 15:41:28 +0200

    Created a new setState for filteredRecipes function in RecipeSearch file, created a handle function to handleFilteredRecipes, then modified the return() passed <RecipeSearch/> with data, clickFn, and the handleFilterRecipe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { RecipeListPage } from "./pages/RecipeListPage";
-// import { RecipeSearch } from "./components/RecipeSearch";
+import { RecipeSearch } from "./components/RecipeSearch";
 import { data } from "./utils/data";
 import { useState } from "react";
 import { RecipePage } from "./components/RecipePage";
@@ -8,15 +8,30 @@ export const App = () => {
   // Your state code here
   const [selectedItem, setSelectedItem] = useState();
 
+  //new state for filtered recipes
+  const [filteredRecipes, setFilteredRecipes] = useState(data.hits);
+  // handle filter recipe from RecipeSearch
+  const handleFilteredRecipes = (recipes) => {
+    setFilteredRecipes(recipes);
+  };
   return (
     <>
       {selectedItem ? (
         <RecipePage item={selectedItem} clickFn={setSelectedItem} />
       ) : (
-        <RecipeListPage selectedItem={selectedItem} clickFn={setSelectedItem} />
+        <>
+          <RecipeSearch
+            data={data}
+            clickFn={setSelectedItem}
+            handleFilteredRecipes={handleFilteredRecipes}
+          />
+          <RecipeListPage
+            selectedItem={selectedItem}
+            clickFn={setSelectedItem}
+            items={filteredRecipes}
+          />
+        </>
       )}
-      {/* <RecipeSearch data={data} clickFn={setSelectedItem} /> */}
-      {/* Recipe Search is rendering the page twice, chek and fix */}
     </>
   );
 };
