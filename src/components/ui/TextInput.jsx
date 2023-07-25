f20552066f0fc09821da09e74c0a45295439dc02
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-25 17:29:54 +0200

    Added margin left

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -3,6 +3,7 @@ import { Input } from "@chakra-ui/react";
 export const TextInput = ({ changeFn, ...props }) => {
   return (
     <Input
+      ml={3}
       variant="outline"
       focusBorderColor="pink.400"
       onChange={changeFn}
