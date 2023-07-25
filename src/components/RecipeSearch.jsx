64337a5c5b944ea90d6a4cc5b2001e31046cea6d
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-25 15:44:21 +0200

    modified files to call a function handleFilteredRecipes from app and passing the matchedRecipes funtion to it. Pass this callback as a prop to the RecipeSearch function

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
-import { data } from "../utils/data";
 import { TextInput } from "./ui/TextInput";
-import { RecipeListPage } from "../pages/RecipeListPage";
 
-export const RecipeSearch = ({ data, clickFn }) => {
+export const RecipeSearch = ({ data, clickFn, handleFilteredRecipes }) => {
   const [searchField, setSearchField] = useState("");
-
-  const matchedRecipes = data.hits.filter((item) => {
-    const { label, dietLabels, cautions, healthLabels } = item.recipe;
-    return label.toLowerCase().includes(searchField.toLowerCase());
-  });
 
   const handleChange = (event) => {
     setSearchField(event.target.value);
+
+    //filter Recipes (searchField)
+    const matchedRecipes = data.hits.filter((item) => {
+      const { label, dietLabels, cautions, healthLabels } = item.recipe;
+      return label.toLowerCase().includes(searchField.toLowerCase());
+    });
+    //function call from App
+    handleFilteredRecipes(matchedRecipes);
   };
 
   return (
     <>
       <label>Search for a recipe:</label>
       <TextInput changeFn={handleChange} w={200} mb={8} />
-      <RecipeListPage clickFn={clickFn} items={matchedRecipes} />
     </>
   );
 };
