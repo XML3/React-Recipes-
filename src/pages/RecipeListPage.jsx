9290479e61ce07524959be177cd96791f0d06ab5
Xagly Montilva <xaglymontilva@gmail.com>
2023-07-25 15:50:29 +0200

    Commented out previous testing code, modified the mapped recipe object, changed to items.map from recipe.map

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,13 +1,12 @@
 import { Center, Heading, SimpleGrid } from "@chakra-ui/react";
-import { data } from "../utils/data";
 import { RecipeItemCard } from "../components/RecipeItemCard";
 
 export const RecipeListPage = ({ items, clickFn }) => {
   // You can play around with the console log, but ultimately remove it once you are done
   //console.log(data.hits[0].recipe.label);
 
-  // Extracted data.hits and placed in recipes variable
-  const recipes = data.hits;
+  // Extracted data.hits and placed in recipes variable (testing)
+  // const recipes = data.hits;
 
   return (
     <>
@@ -22,7 +21,7 @@ export const RecipeListPage = ({ items, clickFn }) => {
 
         <SimpleGrid columns={4} gap={8}>
           {/* Mapped throught recipe object as the "item" prop for RecipeItemCard and used unique Key prop for each recipe item (index or recipe URL) */}
-          {recipes.map((item, index) => (
+          {items.map((item) => (
             <RecipeItemCard
               key={item.recipe.url}
               item={item}
