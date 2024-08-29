36d6e3200791fca4704b90fafac769b1a8b4778b
Xagly Montilva <xaglymontilva@gmail.com>
2024-08-29 14:54:56 +0200

    modified header and svg placements and size

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { RecipeSearch } from "./components/RecipeSearch";
 import { data } from "./utils/data";
 import { useState } from "react";
 import { RecipePage } from "./components/RecipePage";
-import { Box, Center, Heading, Image } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading, Image } from "@chakra-ui/react";
 import recipe from "/img/recipe_bowl.png";
 
 export const App = () => {
@@ -31,60 +31,75 @@ export const App = () => {
 
   return (
     <Box bgColor={"gray.900"} w={"100%"} h={"100%"}>
-      <Box
-        w={{ base: "50%", sm: "25%", md: "10%" }}
-        h={"auto"}
-        bgColor={"gray.900"}
-        position={"relative"}
-        top={{ base: "10px", sm: "50", md: "100px" }}
-        left={{ base: "25%", sm: "10%", md: "20%" }}
-        mb={{ base: "1rem", sm: "1rem", md: 0 }}
-      >
-        <Image
-          src={recipe}
-          alt="wisk and bowl"
-          w={{ base: "75px", sm: "75%", md: "200px" }}
-          h={"auto"}
-        />
+      <Box w={"100%"} h={"auto"}>
+        <Flex
+          flexDir="column"
+          align={"center"}
+          justifyContent={"center"}
+          gap={{ base: 3, sm: 8, md: 12 }}
+          mb={{ base: "1rem", sm: "1rem", md: 0 }}
+        >
+          <Image
+            src={recipe}
+            alt="wisk and bowl"
+            w={{ base: "75px", sm: "100px", md: "150px" }}
+            h={"auto"}
+            mt={"2rem"}
+          />
+          <Heading
+            as="h1"
+            fontFamily={orbitronFontFamily}
+            fontWeight={orbitronWeight.bold}
+            fontSize={["3xl", "4xl", "6xl"]}
+            position={"relative"}
+            left={{ base: "25%", sm: 0, md: 0 }}
+            color={"whitesmoke"}
+          >
+            {header}
+          </Heading>
+        </Flex>
       </Box>
 
-      <Center
-        w={"100%"}
-        h="150hv"
-        flexDir="column"
-        bgColor="gray.900"
-        color="whitesmoke"
-      >
-        <Heading
-          as="h1"
-          fontFamily={orbitronFontFamily}
-          fontWeight={orbitronWeight.bold}
-          fontSize={["3xl", "4xl", "6xl"]}
-          position={"relative"}
-          left={{ base: "10%", sm: "10%", md: 0 }}
+      <Box>
+        <Center
+          w={"100%"}
+          h="150hv"
+          flexDir="column"
+          bgColor="gray.900"
+          color="whitesmoke"
         >
-          {header}
-        </Heading>
+          {/* <Heading
+            as="h1"
+            fontFamily={orbitronFontFamily}
+            fontWeight={orbitronWeight.bold}
+            fontSize={["3xl", "4xl", "6xl"]}
+            position={"relative"}
+            left={{ base: "10%", sm: "10%", md: 0 }}
+            mt={"2rem"}
+          >
+            {header}
+          </Heading> */}
 
-        {selectedItem ? (
-          <RecipePage item={selectedItem} clickFn={setSelectedItem} />
-        ) : (
-          <>
-            <RecipeSearch
-              items={filteredRecipes}
-              clickFn={setSelectedItem}
-              //feedback from Winc: fixing search bug, the following will bring the items back when back-spacing
-              originalItems={data.hits}
-              handleFilteredRecipes={handleFilteredRecipes}
-            />
-            <RecipeListPage
-              selectedItem={selectedItem}
-              clickFn={setSelectedItem}
-              items={filteredRecipes}
-            />
-          </>
-        )}
-      </Center>
+          {selectedItem ? (
+            <RecipePage item={selectedItem} clickFn={setSelectedItem} />
+          ) : (
+            <>
+              <RecipeSearch
+                items={filteredRecipes}
+                clickFn={setSelectedItem}
+                //feedback from Winc: fixing search bug, the following will bring the items back when back-spacing
+                originalItems={data.hits}
+                handleFilteredRecipes={handleFilteredRecipes}
+              />
+              <RecipeListPage
+                selectedItem={selectedItem}
+                clickFn={setSelectedItem}
+                items={filteredRecipes}
+              />
+            </>
+          )}
+        </Center>
+      </Box>
     </Box>
   );
 };
